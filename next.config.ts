16d389f57998c8b9952c15250ae7da11e0ebb85a
B-priyam <priyam3801h@gmail.com>
2025-09-26 14:08:40 +0530

    ui fixed

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   //   basePath: "/Osteocare-Bone-Health-Survey",
   //   assetPrefix: "/Osteocare-Bone-Health-Survey",
+  assetPrefix: "https://bmd-survey.vercel.app",
 };
 
 export default nextConfig;
